e5bf876400b4acbff6f02a2e897ef18af3632511
jdev082 <willsjayden2@gmail.com>
2023-04-13 17:09:46 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const bg = document.querySelector('#bg');
+const td = document.querySelector('#td');
+const clb = document.querySelector('#clb');
+const ctc = document.querySelector('#ctc');
+const sb = document.querySelector("#sb");
+const genbtn = document.querySelector('#genbtn')
+
+function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+
+function generate() {
+    background = bg.value;
+    textdef = td.value;
+    swbtn = sb.value;
+    chlsbg = clb.value;
+    chlscl = ctc.value;
+    json = {
+        "name": "test",
+        "description": "test",
+        "authors": [
+            {
+                "name": "jdev082",
+                "id": "1038089405161807872"
+            }
+        ],
+        "semanticColors": {
+            "BACKGROUND_PRIMARY": [
+                `${background}`
+            ],
+        }
+    }
+    download(json, 'theme.json', 'text/plain');
+}
+
+genbtn.addEventListener('click', generate());
